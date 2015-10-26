0746fa51e2879d145ef71946a793800a11801541
Hal Henke <halhenke@gmail.com>
2015-10-26 20:04:01 +1100

    Swap out package-merge for loads.merge for package.json merging

--- test/lib/mergePack-test.js
+++ test/lib/mergePack-test.js
@@ -7,22 +7,31 @@ test('Merge Packages', t => {
   const packPath = path.join(process.cwd(), 'package.json');
   const templatedFile = JSON.stringify({
     name: 'templatePackage',
-    description: 'This should overwrite stuff'
+    description: 'This should overwrite stuff',
+    main: 'src/index.js',
+    scripts: {
+      start: 'do a lotta stuff',
+      test: 'mocha this package'
+    }
   });
-  const originalFile = JSON.stringify({
+  const originalFile = {
     name: 'originalPackage',
     description: 'This should be overwritten by a template',
     scripts: {
-      start: 'do a lotta stuff'
+      start: 'do a lotta stuff',
+      lint: 'lint this package'
     }
-  });
-  const mergedFile = JSON.stringify({
+  };
+  const mergedFile = {
     name: 'templatePackage',
     description: 'This should overwrite stuff',
+    main: 'src/index.js',
     scripts: {
-      start: 'do a lotta stuff'
+      start: 'do a lotta stuff',
+      lint: 'lint this package',
+      test: 'mocha this package'
     }
-  });
+  };
 
   t.ok(mergePackages instanceof Function, 'should be function');
 
@@ -31,7 +40,9 @@ test('Merge Packages', t => {
   t.deepEqual(mergePackages(false, [packPath])([templatedFile]), [templatedFile],
     'should return the generated package when there is no pre-existing package.json');
 
-  t.deepEqual(mergePackages(originalFile, [packPath])([templatedFile]), [mergedFile],
+  const merger = mergePackages(originalFile, [packPath])([templatedFile])[0];
+
+  t.deepEqual(JSON.parse(merger), mergedFile,
     'should merge an existing package.json with template and return results');
 
   t.end();
